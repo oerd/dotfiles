4f62436f37fdf308166368e5de00ee1d50a7616c
Oerd Cukalla <rosaccu@gmail.com>
2021-01-19 18:48:32 +0100

    More integrations and env vars

---
+++ homebrew/options.zsh
@@ -0,0 +1,4 @@
+# Homebrew security and privacy settings
+HOMEBREW_NO_INSECURE_REDIRECT=1
+HOMEBREW_CASK_OPTS=--require-sha
+HOMEBREW_NO_ANALYTICS=1
---
+++ iterm2/integrations.zsh
@@ -0,0 +1,5 @@
+#
+# INSTALL ITERM2 INTEGRATIONS
+#
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+export PATH="/usr/local/sbin:$PATH"
