c981be1baf679376ff5fd3ce57cb9db2deeb0064
Oerd Cukalla <rosaccu@gmail.com>
2021-01-19 20:59:58 +0100

    Add some common macos packages to be installed.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,20 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+brew_install() {
+  local command=$1
+  local command_with_options=$@
+
+  echo "Command: $command"
+  echo "Command with options: $command_with_options"
+
+  local result=$(which $command)
+  if [ -z $result ]
+  then
+    brew install $command_with_options
+  fi
+}
+
 # Check for Homebrew
 if test ! $(which brew)
 then
@@ -20,5 +34,18 @@ then
   fi
 
 fi
+
+# Install `wget` with IRI support.
+brew_install wget --with-iri
+# Install GnuPG to enable PGP-signing commits.
+brew_install gnupg
+brew_install grep
+brew_install openssh
+brew_install screen
+brew_install git
+brew_install git-lfs
+brew_install ssh-copy-id
+brew_install pyenv
+brew_install nvm
 
 exit 0
