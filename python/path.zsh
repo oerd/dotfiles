c712cb4a592f2480d3b7763b0ad67566db812e12
Oerd Cukalla <rosaccu@gmail.com>
2021-08-17 15:47:32 +0200

    feat(py): improve python tool setup

--- python/path.zsh
+++
@@ -1,4 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-export PIPENV_VENV_IN_PROJECT=1
---
+++ python/tools.zsh
@@ -0,0 +1,9 @@
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+# Pipenv (just ensures env var - no activation)
+export PIPENV_VENV_IN_PROJECT=1
+
+# Pipx-installed binaries
+export PATH="$PATH:$HOME/.local/bin"
