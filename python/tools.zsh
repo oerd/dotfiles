f30742c4389ff65623c0879f4cce3d9865ec36c3
Oerd Cukalla <oerd.cukalla@jimdo.com>
2022-08-29 16:48:20 +0200

    add pyenv

--- python/tools.zsh
+++ python/tools.zsh
@@ -7,3 +7,24 @@ export PIPENV_VENV_IN_PROJECT=1
 
 # Pipx-installed binaries
 export PATH="$PATH:$HOME/.local/bin"
+
+export PYENV_SHELL=zsh
+PYENV_VER=$(pyenv --version | cut -d" " -f2)
+source "/usr/local/Cellar/pyenv/${PYENV_VER}/libexec/../completions/pyenv.zsh"
+command pyenv rehash 2>/dev/null
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")"
+    ;;
+  *)
+    command pyenv "$command" "$@"
+    ;;
+  esac
+}
