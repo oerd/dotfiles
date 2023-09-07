9275e8624189de3d4b86a0421dd924b972bfdc8c
Oerd Cukalla <oerd.cukalla@jimdo.com>
2023-09-07 02:18:20 +0200

    Add golang auto-installation

--- go/install.sh
+++ go/install.sh
@@ -1,7 +1,10 @@
+
 if test ! $(which powerline-go)
 then
-  if test $(which go)
+  if test ! $(which go)
   then
+    brew install go
+  else
     go get -u github.com/justjanne/powerline-go
   fi
 fi
