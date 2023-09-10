dfec7e0357d5c5f32529e5a661b3217d84e56c06
Oerd Cukalla <oerd.cukalla@jimdo.com>
2023-09-10 15:24:12 +0200

    Sort homebrew installables.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -32,66 +32,63 @@ then
 
 fi
 
-# Install `wget` with IRI support.
-brew_install wget
+brew_install asdf
+brew_install bat
 brew_install fzf
-# Install GnuPG to enable PGP-signing commits.
-brew_install gnupg
-brew_install grep
-brew_install openssh
-brew_install screen
+brew_install gh
 brew_install git
 brew_install git-lfs
-brew_install gh
-brew_install ssh-copy-id
-brew_install pyenv
-brew_install pipx
-brew_install asdf
-brew_install neovim
-brew_install starship
-brew_install volta
-brew_install zsh
-brew_install ne
+brew_install gnupg
+brew_install grep
 brew_install jq
-brew_install yq
-brew_install awscli
-brew_install terraform
 brew_install mas
-
+brew_install ne
+brew_install neovim
+brew_install openssh
+brew_install pipx
+brew_install pyenv
+brew_install screen
+brew_install ssh-copy-id
+brew_install starship
+brew_install terraform
+brew_install volta
+brew_install wget
+brew_install yq
+brew_install zsh
 
 # Install Casks
+brew install --cask 1password
 brew install --cask brave-browser
 brew install --cask coconutbattery
-brew install --cask docker
 brew install --cask discord
-brew install --cask jetbrains-toolbox
+brew install --cask docker
 brew install --cask goland
 brew install --cask intellij-idea
 brew install --cask iterm2
+brew install --cask jetbrains-toolbox
 brew install --cask lastpass
-brew install --cask 1password
+brew install --cask meetingbar
 brew install --cask monitorcontrol
+brew install --cask postman
 brew install --cask pycharm
 brew install --cask rectangle
-brew install --cask meetingbar
 brew install --cask secretive
 brew install --cask slack
 brew install --cask sublime-text
 brew install --cask visual-studio-code
-brew install --cask postman
 brew install --cask zed
 
 ## some common taps:
 brew tap homebrew/cask-fonts
 
 ## install fonts
-brew_install font-fira-mono-for-powerline
-brew_install font-source-code-pro-for-powerline
-brew_install font-fira-code-nerd-font
 brew_install font-anonymice-nerd-font
+brew_install font-fira-code-nerd-font
+brew_install font-fira-mono-for-powerline
 brew_install font-jetbrains-mono-nerd-font
 brew_install font-jetbrains-mono-nerd-font
 brew_install font-sauce-code-pro-nerd-font
+brew_install font-source-code-pro-for-powerline
 
 ## install apps via the Mac App Store
 # requires mas-cli
