075a457f1473b150f878a5172a7652655853b65f
Oerd Cukalla <oerd.cukalla@jimdo.com>
2023-09-10 12:32:21 +0200

    reset homebrew location etc.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,8 +10,12 @@
 # Disable press-and-hold for keys in favor of key repeat.
 # defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
 # Use AirDrop over every interface. srsly this should be a default.
-# defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
