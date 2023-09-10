8b86c1503b22313dbe4504109e013175a5330b5c
Oerd Cukalla <oerd.cukalla@jimdo.com>
2023-09-10 12:38:56 +0200

    Fix path to include homebrew by default.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,8 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+if [[ $(uname -p) = 'arm' ]]; then
+  export PATH="/opt/homebrew/bin:$PATH"
+else
+  export PATH="/usr/local/bin:$PATH"
+fi
