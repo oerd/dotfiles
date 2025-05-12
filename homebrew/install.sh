1750f13eea017b6a49e4b68956cce92e324c744d
Oerd Cukalla <rosaccu@gmail.com>
2025-05-12 17:37:07 +0200

    Update homebrew items

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,17 +5,6 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-brew_install() {
-  local command=$1
-  local command_with_options=$@
-
-  local result=$(which $command)
-  if [ -z $result ]
-  then
-    brew install $command_with_options
-  fi
-}
-
 # Brew is required
 if test ! $(which brew)
 then
@@ -38,32 +27,29 @@ fi
 brew tap FelixKratz/formulae # sketchybar
 brew tap nikitabobko/tap     # aerospace
 
-brew_install asdf
-brew_install bat
-brew_install fzf
-brew_install gh
-brew_install git
-brew_install git-lfs
-brew_install gnupg
-brew_install grep
-brew_install jq
-brew_install mas
-brew_install ne
-brew_install neovim
-brew_install openssh
-brew_install pipx
-brew_install pyenv
-brew_install screen
-brew_install ssh-copy-id
-brew_install starship
-brew_install terraform
-# brew_install volta
-brew_install wget
+brew insall 1password-cli
+brew install asdf
+brew install bat
+brew install fzf
+brew install gh
+brew install git
+brew install git-lfs
+brew install gnupg
+brew install grep
+brew install hashicorp/tap/terraform
+brew install jq
+brew install mas
+brew install ne
+brew install neovim
+brew install openssh
+brew install pyenv
+brew install screen
+brew install ssh-copy-id
+brew install starship
+brew install wget
 brew install uv
-brew_install yq
-brew_install zsh
-brew install sketchybar
-brew install aerospace
+brew install yq
+brew install zsh
 
 # Install Casks
 brew install --cask 1password
@@ -71,7 +57,6 @@ brew install --cask brave-browser
 brew install --cask coconutbattery
 brew install --cask discord
 brew install --cask ghostty
-brew install --cask iterm2
 brew install --cask jetbrains-toolbox
 brew install --cask meetingbar
 # brew install --cask monitorcontrol
@@ -83,13 +68,15 @@ brew install --cask sublime-text
 brew install --cask zed
 
 ## install fonts
-brew_install font-anonymice-nerd-font
-brew_install font-fira-code-nerd-font
-brew_install font-fira-mono-for-powerline
-brew_install font-jetbrains-mono-nerd-font
-brew_install font-jetbrains-mono-nerd-font
-brew_install font-sauce-code-pro-nerd-font
-brew_install font-source-code-pro-for-powerline
+brew install font-anonymice-nerd-font
+brew install font-fira-code-nerd-font
+brew install font-fira-mono-for-powerline
+brew install font-jetbrains-mono-nerd-font
+brew install font-jetbrains-mono-nerd-font
+brew install font-sauce-code-pro-nerd-font
+brew install font-source-code-pro-for-powerline
+# Monaspace from Github-Next includes nerd-fonts
+brew install font-monaspace
 
 ## install apps via the Mac App Store
 # requires mas-cli
