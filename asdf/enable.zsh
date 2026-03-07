4cdf22e6e181ea0d3865ef0d5b5f0fd4921e01ec
Oerd Cukalla <rosaccu@gmail.com>
2026-03-07 23:48:13 +0100

    Cleanup

--- asdf/enable.zsh
+++
@@ -1,4 +0,0 @@
-ASDF_SH="$(brew --prefix asdf)/libexec/asdf.sh"
-if [ -f $ASDF_SH ]; then
-    source $ASDF_SH
-fi
--- volta/enable.zsh
+++
@@ -1,2 +0,0 @@
-export VOLTA_HOME=${VOLTA_HOME:-~/.volta}
-#export PATH=$VOLTA_HOME/bin:$PATH
