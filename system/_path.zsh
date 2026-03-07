d89476d89774bf33745386dce8be716766bb7659
Oerd Cukalla <rosaccu@gmail.com>
2026-03-07 23:58:48 +0100

    Edits

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,8 +1,2 @@
-export PATH="~/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:~/.local/bin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-if [[ $(uname -p) = 'arm' ]]; then
-  export PATH="/opt/homebrew/bin:$PATH"
-else
-  export PATH="/usr/local/bin:$PATH"
-fi
